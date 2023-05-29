d12d42c6086d913034d03d817f8df072dd6ced56
Ivaylo <ivaylo@starcoders.com>
2023-05-29 19:41:28 +0300

    2023 fix

---
+++ header.blade.php
@@ -0,0 +1,8 @@
+<h1>WELCOME TO PHODAR SERIES AWARD!</h1>
+<h2>2021 Grand Prize is $2.000</h2>
+<h5>DEADLINE IS APRIL 11TH, 2021</h5>
+<h5>ENTER NOW</h5>
+
+<h1>{{ __('submissions.test') }}</h1>
+
+{{ __('submissions.mest') }}
